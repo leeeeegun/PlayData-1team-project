366d035df1d9047ce32842050487a5cdcbb4e817
baejina10 <bja88841785@gmail.com>
2025-05-28 11:18:15 +0900

    000

--- src/Controller/payment/PaymentView.java
+++ src/Controller/payment/PaymentView.java
@@ -1,28 +1,46 @@
 package Controller.payment;
 
+import java.util.ArrayList;
 import java.util.Scanner;
-
 import dao.payment.PaymentDao;
+import dto.payment.PaymentDTO;
 
 public class PaymentView {
+	public static void payInfo() {//결제 정보 화면에 보여주는거
+		
+	}
 	
-	public void paymentSystem() {
-		
-		Scanner key = new Scanner(System.in);
+	public static void paymentSystem() {
 		PaymentDao pay = new PaymentDao();
-		
+		Scanner sc = new Scanner(System.in);
+		//강의 선택
 		System.out.println("=== 결제 화면 ===");
 		System.out.println("선택한 강의:");
-		System.out.println( title + " | 금액: "+ );
-		
-		System.out.println("총 결제 금액: ");
+		int id = sc.nextInt(); //강의선택
+		//선택한 강의 목록
+		String title = "";
+		int price = 0;
+		ArrayList<PaymentDTO> list = pay.getLecturesPrice(id);
+		for(PaymentDTO lectur : list) {
+			title = lectur.getTitle();
+			price = lectur.getPrice();
+		}
+		System.out.println( "강의명: "+title+" | 금액: "+price +"원");
+		System.out.println("====================================");
+		System.out.println("유저 선택:");
+		int userId = sc.nextInt();
+		int userMoney = pay.userMoney(userId);
+		System.out.println("현재 잔여 포인트: "+userMoney);//--> 유저를 정하는걸 어떻게 하지?
+		System.out.println("총 결제 금액: "+price);
 		
 		System.out.println("결제하시겠습니까? 1.예 2.아니오");
-		int result = key.nextInt();
+		int result = sc.nextInt();
 		if(result==1) {
-			System.out.println("결제가 완료되었습니다");
+			System.out.println("결제가 완료되었습니다.");
+			System.out.println("====================================");
+			System.out.println("현재 회원님의 포인트는 "+(userMoney-price));
 		} else {
-			System.out.println("결제가 취소되었습니다.");
+			System.out.println("결제가 취소되었습니다. 다시 결제 페이지로 돌아갑니다");
 		}
 	}
 }
---
+++ src/dao/userinfo/UserInfoDao.java
@@ -0,0 +1,85 @@
+package dao.userinfo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import dto.userinfo.UserInfoDTO;
+import util.DBUtill;
+import java.sql.Date;
+
+public class UserInfoDao {
+	// 회원 정보 조회
+    public ArrayList<UserInfoDTO> getUserInfo(String login_id, String password) {
+    	Connection con = null;
+        PreparedStatement ptmt = null;
+        ResultSet rs = null;
+        String sql = "select login_id, name, birth_date, phone, money, grade,"+
+        											"total_watch_time from user where login_id = ? and password = ? ";
+        ArrayList<UserInfoDTO> userInfolist = new ArrayList<UserInfoDTO>();
+        try {
+        	con = DBUtill.getConnection();
+			ptmt = con.prepareStatement(sql);
+			ptmt.setString(1, login_id);
+			ptmt.setString(2, password);
+			rs = ptmt.executeQuery();
+            if (rs.next()) {
+            	UserInfoDTO userInfo = new UserInfoDTO(rs.getString(1),
+						 rs.getString(2), Date.valueOf(rs.getDate(3).toLocalDate()),
+						 rs.getString(4), 
+						rs.getInt(5),rs.getString(6),rs.getInt(7));
+            	userInfolist.add(userInfo);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+        	DBUtill.close(rs, ptmt, con);
+        }
+        return userInfolist;
+    }
+    //회원정보수정
+    public int updateUserInfo(UserInfoDTO userInfo) {
+    	String sql = "update user set password=?, name=?, password=?, birth_date=?, phone=?"
+    											+ " where id =? ";
+    	
+		Connection con = null;
+		PreparedStatement ptmt = null;
+		int result = 0;
+		try {
+			con = DBUtill.getConnection();
+			ptmt = con.prepareStatement(sql);
+			ptmt.setInt(1, userInfo.getId());
+			ptmt.setString(2, userInfo.getPassword());
+			ptmt.setString(3, userInfo.getName());
+			ptmt.setDate(4, Date.valueOf(userInfo.getBirth_date()));
+			ptmt.setString(5, userInfo.getPhone());
+			result = ptmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// 자원반납
+			DBUtill.close(null, ptmt, con);
+		}
+		return result;
+	}
+    public int deleteUserInfo(int id) {
+		Connection con = null;
+		PreparedStatement ptmt =  null;
+		int result = 0;
+		String sql = "delete from user where id = ?";
+		
+		try {
+			con = DBUtill.getConnection();
+			ptmt = con.prepareStatement(sql);
+			ptmt.setInt(1, id);
+			result = ptmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBUtill.close(null, ptmt, con);
+		}
+		return result;
+    }
+}
