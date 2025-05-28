031b2224200d30891f9eefad80842626bacc36c6
playdata2 <playdata2@playdata>
2025-05-28 12:44:18 +0900

    25.05.28(화)

--- src/MainLee2.java
+++ src/MainLee2.java
@@ -1,11 +1,16 @@
 import Controller.add.AddController;
+import Controller.grade.GradeController;
 import dto.add.AddDTO;
+import dto.user.UserDTO;
 
 public class MainLee2 {
-	
+
 	public static void main(String[] args) {
+
+//		AddController.addEditor();
 		
-		AddController.addEditor();
-		
+		UserDTO userDTO = new UserDTO(4, "박명수", 0, "입문자");
+		GradeController.gradeUpdate(userDTO, 3600);
+
 	}
 }
